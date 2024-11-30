3a417c096578ad6302e2ff6beaafa7f4368b3ccd
Asif Ikbal Mondal <asikbal7ai@gmail.com>
2024-11-30 17:58:26 +0530

    added new file

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const http=require("http");
 const express=require("express");
 const exp = require("constants");
 const path = require("path");
+const {Server}=require("socket.io");
+
 
 const app=express();
 const server=http.createServer(app);
+const io=new Server(server);
  
 app.use(express.static(path.resolve("./public"))); 
 
+//socket.io
+io.on("connection",(Socket)=>{
+    console.log("New user Connect",Socket.id);
+})
+
 app.get("/",(req,res)=>{
-    return res.sendFile("/public/index.html")
+    return res.sendFile("./public/index.html")  
 })
 
 app.listen(9000,()=>console.log("Server started At",9000));
